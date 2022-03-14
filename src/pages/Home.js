b24d2f2bee4d6fc2ccd64c07e2bd5ec3f3175485
Emilien Baudet <emilien.baudet@gmail.com>
2022-03-14 16:41:15 +0100

    add images-> css:relative jsx:from public folder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 // similar to function Home()
 const Home = () => {
   return (
     <div className="home">
+      <Logo />
       <Navigation />
       <h1>Accueil</h1>
     </div>
