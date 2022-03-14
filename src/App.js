a9ef7cebc671eca0aaf267b8956ee91198973993
Emilien Baudet <emilien.baudet@gmail.com>
2022-03-14 14:12:00 +0100

    ading basis modules and ordering image files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload it, et ça c'est vraiment cool.
         </p>
         <a
           className="App-link"
